8704db910b32e0df24b26cf7ca0d977dc698d777
sykora <sykora@lucentbeing.com>
2008-10-16 20:14:21 +0530

    Tightened formatting loop, added test case.

--- formatter.py
+++ formatter.py
@@ -3,14 +3,16 @@
 # http://lucentbeing.com
 
 # TODO: Get this from optparse
-delimiter = ' '
+inputDelimiter = ' '
+outputDelimiter = ' '
+
 
 line = raw_input()
 lines = []
 
 # Get input.
 while True:
-    lines.append(line.split(delimiter))
+    lines.append(line.split(inputDelimiter))
     try :
         line = raw_input()
     except EOFError :
@@ -23,11 +25,27 @@ widths = []
 # Size of column = size of the largest entry in that column, + 1
 # TODO: Add delimiter support.
 for i in range(numColumns) :
-    widths.append(max(len(line[i]) for line in lines) + 1)
+    widths.append(max(len(line[i]) for line in lines))
 
-formatString = ""
-for i in range(numColumns) :
-    formatString += "%%%ds" % widths[i]
+formatList = ["%%%ds"] * numColumns
 
-for i in lines :
-    print formatString % tuple(i)
+# There has got to be a more efficient way (Time and Space) of doing this.
+# for i in range(len(lines)) :
+#     output = formatList[:]
+#     for j in range(numColumns) :
+#         output[j] %= widths[j]
+#         output[j] %= lines[i][j]
+#     print outputDelimiter.join(output)
+
+for i in range(len(lines)) :
+    output = formatList[:]
+    output = [((s % widths[j]) % lines[i][j])
+              for j, s in enumerate(formatList)]
+    print outputDelimiter.join(output)
+
+
+# for i in range(numColumns) :
+#     formatString += "%%%ds" % widths[i]
+
+# for i in lines :
+#     print formatString % tuple(i)
